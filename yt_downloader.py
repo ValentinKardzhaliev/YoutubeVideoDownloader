4bf5267bde2358e16000d2c514616a46124fae24
Valentin Kardzhaliev <121317495+ValentinKardzhaliev@users.noreply.github.com>
2023-08-20 16:44:14 +0300

    Update yt_downloader.py

--- yt_downloader.py
+++ yt_downloader.py
@@ -5,6 +5,6 @@ link = argv[1]
 
 yt = YouTube(link)
 
-yd = yt.streams.get_highest_resolution()
+yd = yt.streams.get_highest_resolution() # You can set lower resolutions too. Go to README if you wish to know more
 
-yd.download("Downloads")
+yd.download("Downloads" # Provide the path you wish to download the video to
